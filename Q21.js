97a13275797851d29811f8086613b5b13fa1e90d
Faiza Rao <159902088+faizarao2@users.noreply.github.com>
2024-02-21 19:47:56 +0500

    Add files via upload

---
+++ Q21.js
@@ -0,0 +1,3 @@
+// 21. They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+var person = { name: "faiza", Fname: "Rao", age: 20 };
+console.log(person);
